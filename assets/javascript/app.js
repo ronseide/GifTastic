404c77efd915ce2e4b556ce44305f9f07a683b0f
ronseide <ron.seide@gmail.com>
2019-03-07 12:13:08 -0500

    All seems well

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,16 @@
 
 var giphyKey = "kv6Mw2Ll9SuxeytS70UyQpHGIsKzvZot";
 var topics = ["Speed Racer", "The Flintstones", "The Jetsons", "Rocky and Bullwinkle"];
-
 var gifData = [];
-
+// Functions to be called after (document).ready.
 function displayButtons () {
   $("#buttons").text("");
   for (var i = 0; i < topics.length; i++) {
-    $("#buttons").append('<div class="container"><label><input type="button" name="topics" value="' + topics[i] + '" onclick="clicked(' + i + ');" /> ' + '</label></div>');
+    $("#buttons").append('<div class="container"><label><input type="button" class="btn btn-light" name="topics" value="' + topics[i] + '" onclick="clicked(' + i + ');" /> ' + '</label></div>');
   }
 };
 function displayGif(num) {
-  for(var i=0;i<3;i++) {
+  for(var i = 0; i< 3; i++) {
     if(i != num) {
       var gifStillUrl = gifData[i].images.fixed_height_still.url;
       $("#imgGraph"+i).attr("src", gifStillUrl);
@@ -20,12 +19,10 @@ function displayGif(num) {
   var fullGif = gifData[num].images.fixed_height.url;
   $("#imgGraph"+num).attr("src", fullGif);
 }
-
 function clicked(id) {
   var searchToon = topics[id];
   var searchToonTrimmed = searchToon.trim().replace(/ /g, "+");
   var url_base = "https://api.giphy.com/v1/gifs/search?q=" + searchToonTrimmed + "+Cartoon&api_key=" + giphyKey + "&limit=3"
-  console.log("AJAX request to: " + url_base);
   $.ajax({
     url: url_base,
     method: "GET"
@@ -37,14 +34,10 @@ function clicked(id) {
     }
   });
 };
-
 $(document).ready(function () {
   $("#submit").on("click", function() {
-    console.log("Submit is clicked");
     var inputValue = $("#userInput").val();
-    console.log(inputValue);
     topics.push(inputValue);
-    console.log(topics);
     displayButtons ();
   });
   displayButtons ();
